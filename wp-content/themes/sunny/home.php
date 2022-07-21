cb785243f9866434332ff7845e76e6f75348ae8c
sunny <geek92sunny@gmail.com>
2022-07-21 22:45:44 +0530

    up to loading posts templates on home

--- wp-content/themes/sunny/home.php
+++ wp-content/themes/sunny/home.php
@@ -13,4 +13,10 @@ $context = Timber::context();
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
+
+$context['all_posts'] = Timber::get_posts([
+							'post_type' 		=> 'post',
+							'posts_per_page'	=> 3	
+						]);
+
 Timber::render( array( 'home.twig', 'page.twig' ), $context );
--- wp-content/themes/sunny/theme.php
+++ wp-content/themes/sunny/theme.php
@@ -22,22 +22,10 @@ class Theme extends \Timber\Site {
     public function __construct() {
         add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
         add_filter( 'timber/context', array( $this, 'add_to_context' ) );
+        add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
         add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
 
         parent::__construct();
-    }
-
-
-    /**
-     * This is where you add some context
-     * 
-     * @param string $context context['this'] Being the Twig's {{ this }}.
-     */
-    public function add_to_context( $context ) {
-        // $context['menu']  = new \Timber\Menu();
-        $context['site']  = $this;
-
-        return $context;
     }
 
     public function theme_supports() {
@@ -59,18 +47,50 @@ class Theme extends \Timber\Site {
         add_theme_support( 'menus' );
     }
 
+    /**
+     * This is where you add some context
+     * 
+     * @param string $context context['this'] Being the Twig's {{ this }}.
+     */
+    public function add_to_context( $context ) {
+        // $context['menu']  = new \Timber\Menu();
+        $context['site']  = $this;
+
+        return $context;
+    }
+
+    /**
+     * Custom functions to twig
+     * 
+     * @param object the twig object
+     * @return object the twig object
+     */
+    public function add_to_twig($twig){
+        $twig->addFunction( new \Timber\Twig_Function( 'carbon_get_theme_option', 'carbon_get_theme_option' ) );
+
+        return $twig;
+    }    
+
     public function scripts(){
         $this->css();
         $this->js();
     }
 
     protected function css(){
+        $theme_url = get_template_directory_uri();
+        
         wp_enqueue_style(
             "{$this->theme_name}-bootstrap",
             "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+            array()
+        );
+
+        wp_enqueue_style(
+            "{$this->theme_name}-main",
+            "{$theme_url}/assets/css/main.css",
             array(),
             $this->script_ver
-        );
+        );        
     }
 
     protected function js(){
